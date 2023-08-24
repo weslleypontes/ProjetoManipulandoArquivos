70fcdd9c35d0bb04f438665f3442c482a4e542e3
Weslley Pontes <contato.weslleypontes@gmail.com>
2023-08-24 16:33:32 -0300

    Adicionada contexto de banco de dados

---
+++ LeituraArquivos/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LeituraArquivos.Models;
+namespace LeituraArquivos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NFe> NFes { get; set; }
+    }
+}
--- LeituraArquivos/Program.cs
+++ LeituraArquivos/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using LeituraArquivos.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext"), builder =>
+                        builder.MigrationsAssembly("LeituraArquivos")));
 
 var app = builder.Build();
 
